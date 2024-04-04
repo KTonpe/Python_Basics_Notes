2e230b821feef292ca066dec546791dcb503773d
KTonpe <ktonpe.2002@gmail.com>
2024-04-04 11:49:18 +0530

    added Calender

--- Leap_year.py
+++ Leap_year.py
@@ -7,12 +7,16 @@ The condition for Leap year are :
 so point (1,2) -> AND ; point 3 -> OR
 '''
 
+import calendar
 
+year =   int(input('Enter the Year in number format: '))
+month  = int(input('Month in number format: '))
 
-year = int(input('Enter the Year in number format: '))
-
-result = f'{year} is a leap year' if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) else f'{year} is not a leap year'
-print(result)
+if year > 0: #here year must be INT 
+    result = f'{year} is a leap year' if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) else f'{year} is not a leap year'
+    print(result)
+    print(calendar.month(year, month)) if (month > 0 and month < 13) else print(f'give a valid {month}')
+else : print(f'Enter a valid year')       
 
 
 '''if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0): print(f'{year} is a leap year')
