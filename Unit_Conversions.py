ac5c417b2bdd84f2c5d484526835ca1ea1b7a281
KTonpe <ktonpe.2002@gmail.com>
2024-04-04 09:50:52 +0530

    Added 'c to 'f

--- Unit_Conversions.py
+++ Unit_Conversions.py
@@ -2,11 +2,11 @@
 Used round( , x) function to round off the float to x decimals 
 '''
 
-# km = float(input('Enter the kilo meters to convert into Miles : '))
-# #conversion factor from km to miles
-# con_from_km_to_miles = 0.621371
-# miles = km * con_from_km_to_miles
-# print(f'Conversion of {km}KM is  {round(miles,2)} miles')
+km = float(input('Enter the kilo meters to convert into Miles : '))
+#conversion factor from km to miles
+con_from_km_to_miles = 0.621371
+miles = km * con_from_km_to_miles
+print(f'Conversion of {km}KM is  {round(miles,2)} miles')
 
 
 celsius = float(input('Enter the temperature in Celsius: '))
