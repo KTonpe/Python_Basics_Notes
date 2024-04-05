6bb7a714074576805eda052174981351bca5eb02
KTonpe <ktonpe.2002@gmail.com>
2024-04-05 12:51:59 +0530

    facts and desecd

--- Accsending_order.py
+++ Accsending_order.py
@@ -1,3 +1,10 @@
+'''
+1.you can't directly add a number in a empty list
+2. if you eant to append without using it -> ypu you add the lists : list_name = list_name + [x]
+3. please check the index as it starts from 0 -> so length must be x+1
+'''
+
+
 limit = int(input("Enter the limit: "))
 #print(limit)
 list_of_numbers = []
@@ -5,14 +12,15 @@ for i in range(0,limit):
     #list_of_numbers.append(int(input("Enter the number: ")))
     num = int(input("Enter the number: "))
     list_of_numbers += [num]
-    
 
-list_of_numbers.sort()
+list_of_numbers.sort() #arranged in asscending order
+print(list_of_numbers)
+list_of_numbers.sort(reverse=True) #arranged in descending order
 print(list_of_numbers)
 
 location = int(input('Enter the location: '))
 number = int(input('Enter the number: '))
-list_of_num =[0]*(location+1)
+list_of_num =[0]*(location+1) #adding 0s in the list of length location+1
 list_of_num[location] = number 
 
-print(list_of_num)
+print(list_of_num,len(list_of_num))
